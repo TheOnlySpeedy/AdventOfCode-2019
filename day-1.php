3b0683a6f6a51f8902c2f26636202c67add1366f
Florian Schmid <f.schmid@agentur-dreibein.de>
2019-12-01 15:36:17 +0100

    Change required amount for rekursion loop

--- day-1.php
+++ day-1.php
@@ -5,7 +5,7 @@ $input = getInputForDay(1);
 function getFuel($mass, $fuelForFuel = false)
 {
     $fuel = floor($mass / 3) - 2;
-    if ($fuelForFuel && $fuel > 6) {
+    if ($fuelForFuel && $fuel > 8) {
         $fuel += getFuel($fuel, true);
     }
     return $fuel;
